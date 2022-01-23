6dc9ceb6ae0b84734de505b8e101dff75251d0bc
Junyoung Lee <jack04215@naver.com>
2022-01-23 23:34:50 +0900

    20220123 juny

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-import sys
-input = sys.stdin.readline
-
-S = input().rstrip()
-
-print(S)
+s = set([])
+M = 'ABA'
+m = len(M)
+for j in range(m-1-0):
+    _x = M[j]+M[j+1+0]
+    s.add(_x)
+print(len(s))
